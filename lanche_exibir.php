095b34cafe7a842b2da94b1bd06e0adb6bf1e434
regilanmeira <regilan@hotmail.com>
2023-11-23 08:31:23 -0300

    Exibição dos dados do lanche

--- lanche_exibir.php
+++ lanche_exibir.php
@@ -5,9 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exibir lanche</title>
     <link href="estilo.css" rel="stylesheet">
-    
+    <script src="https://kit.fontawesome.com/aa1f2d8e44.js" crossorigin="anonymous"></script>
 </head>
 <body>
+<h1>Lanches disponíveis</h1>
+<table>
+    <tr>
+        <th>Descrição</th>
+        <th>Preço R$ </th>
+        <th>Ações</th>
+
+    </tr>
+
+
+    <?php 
+        include "conexao_bd.php";
+
+        $sql = "SELECT * FROM lanche ORDER BY descricao";
+
+        $resultado = retornarDados($sql);
+
+        while ($linha = mysqli_fetch_assoc($resultado))
+        {
+   
+           
+    ?>
+
+    <tr>
+            <td><?php echo $linha["descricao"] ?></td>
+            <td><?php echo $linha["preco"] ?></td>
+            <td><i class="fa-solid fa-eye fa-beat" style="color: #30c549;"></i></td>
+    </tr>
+
+    <?php
+        }
+    ?>
+
+
+</table>
     
 </body>
 </html>
